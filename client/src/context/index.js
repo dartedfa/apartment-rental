7fbdc228a640b872503905a8b14a1b406a411f82
Gio Gelashvili <giorgi.gelashvili128@gmail.com>
2021-03-12 03:00:52 +0400

    Added React-query dev tools

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -1,8 +1,14 @@
 import * as React from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
+import {ReactQueryDevtools} from 'react-query-devtools'
 
 function AppProviders({children}) {
-  return <Router>{children}</Router>
+  return (
+    <Router>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </Router>
+  )
 }
 
 export default AppProviders
