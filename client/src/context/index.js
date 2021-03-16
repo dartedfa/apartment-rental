9f778443e5bef48ccb217eec6def266d5017a95a
Gio Gelashvili <giorgi.gelashvili128@gmail.com>
2021-03-16 18:35:50 +0400

    Add history provided by history package and export it from appProviders.

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -2,10 +2,13 @@ import * as React from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import {ReactQueryDevtools} from 'react-query-devtools'
 import {AuthProvider} from './auth-context'
+import {createBrowserHistory} from 'history'
+
+export const history = createBrowserHistory()
 
 function AppProviders({children}) {
   return (
-    <Router>
+    <Router history={history}>
       <ReactQueryDevtools initialIsOpen={false} />
       <AuthProvider>{children}</AuthProvider>
     </Router>
