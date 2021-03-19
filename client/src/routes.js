955e90533e581c92a530cd396b1b32171d846ade
Gio Gelashvili <giorgi.gelashvili128@gmail.com>
2021-03-19 23:59:39 +0400

    Added routes with restrictions.

--- client/src/routes.js
+++ client/src/routes.js
@@ -5,10 +5,29 @@ import {useAuth} from './context/auth-context'
 function AppRoutes() {
   const {user} = useAuth()
   const isAdmin = user.role === 2
+  const isRealtor = user.role >= 1
   return (
     <Routes>
       <Route path="/apartments" element={<div>Apartment List</div>} />
+
+      {isRealtor && (
+        <Route
+          path="/apartments/edit/:id"
+          element={<div>Edit Apartment</div>}
+        />
+      )}
+      {isRealtor && (
+        <Route path="/apartments/new" element={<div>Add Apartment</div>} />
+      )}
+
+      <Route path="/apartments/:id" element={<div>View Apartment</div>} />
+
+      <Route path="/account" element={<div>Account</div>} />
+
       {isAdmin && <Route path="/users" element={<div>Users List</div>} />}
+      {isAdmin && <Route path="/users/:id" element={<div>User Edit</div>} />}
+      {isAdmin && <Route path="/users/new" element={<div>Create User</div>} />}
+
       <Route path="*" element={<NotFoundScreen />} />
     </Routes>
   )
