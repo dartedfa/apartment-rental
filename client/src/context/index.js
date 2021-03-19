ac9d6a315000d3d1e826d623d4fcb440ab2cf10e
Gio Gelashvili <giorgi.gelashvili128@gmail.com>
2021-03-19 23:07:21 +0400

    Reverted react-query to older version.

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -3,15 +3,29 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import {ReactQueryDevtools} from 'react-query-devtools'
 import {AuthProvider} from './auth-context'
 import {createBrowserHistory} from 'history'
+import {ReactQueryConfigProvider} from 'react-query'
 
 export const history = createBrowserHistory()
 
+const queryConfig = {
+  queries: {
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false,
+    retry(failureCount, error) {
+      if (error.status === 404) return false
+      else return failureCount < 2
+    },
+  },
+}
+
 function AppProviders({children}) {
   return (
-    <Router history={history}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <AuthProvider>{children}</AuthProvider>
-    </Router>
+    <ReactQueryConfigProvider config={queryConfig}>
+      <Router history={history}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <AuthProvider>{children}</AuthProvider>
+      </Router>
+    </ReactQueryConfigProvider>
   )
 }
 
