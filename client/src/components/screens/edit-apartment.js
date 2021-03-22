f8d7283147a212be47d6da2d31eaa12f2ea9f7e3
Gio Gelashvili <giorgi.gelashvili128@gmail.com>
2021-03-23 02:39:39 +0400

    removed unnecessary dependencies

--- client/src/components/screens/edit-apartment.js
+++ client/src/components/screens/edit-apartment.js
@@ -1,26 +1,12 @@
 /** @jsxRuntime classic /
  /** @jsx jsx */
 import {jsx} from '@emotion/react'
-import * as React from 'react'
+// import * as React from 'react'
 import ApartmentForm from '../organisms/forms/apartment-form'
-import {
-  useApartment,
-  useRemoveApartment,
-  useUpdateApartment,
-} from '../../utils/apartments'
+import {useApartment, useUpdateApartment} from '../../utils/apartments'
 import {useParams} from 'react-router-dom'
 import {useNavigate} from 'react-router'
 import {FullPageSpinner} from '../atoms/Spinner'
-import {ActionButton} from '../atoms/Button'
-import {FaMinusCircle} from 'react-icons/all'
-
-const testData = {
-  _id: '60563bfa45d46a811070d36a',
-  description: 'Test it updates !',
-  price: 100,
-  longitude: '44.7048628',
-  latitude: '41.7221374',
-}
 
 function EditApartmentScreen() {
   const navigate = useNavigate()
