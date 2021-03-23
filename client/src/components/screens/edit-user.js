7bc5d651644840f67abe70cea42e109e9ff93cd4
Gio Gelashvili <giorgi.gelashvili128@gmail.com>
2021-03-23 17:35:24 +0400

    Add logic to change avatar for users.

--- client/src/components/screens/edit-user.js
+++ client/src/components/screens/edit-user.js
@@ -4,6 +4,8 @@ import {useUpdateUser, useUser} from '../../utils/users'
 import {FullPageSpinner} from '../atoms/Spinner'
 import * as React from 'react'
 import UserForm from '../organisms/forms/UserForm'
+import UploadAvatar from '../molecules/upload-avatar'
+import {useState} from 'react'
 
 function EditUserScreen() {
   const navigate = useNavigate()
@@ -11,6 +13,7 @@ function EditUserScreen() {
 
   const {data: user = {}, isLoading, error} = useUser(userId)
   const [handleSubmitUpdate] = useUpdateUser({throwOnError: true})
+  const [avatar, setAvatar] = useState(user.avatar || '')
 
   if (isLoading) {
     return <FullPageSpinner />
@@ -19,11 +22,17 @@ function EditUserScreen() {
   return (
     <>
       <h1>Edit User</h1>
+      {user.userType === 'regular' && (
+        <UploadAvatar
+          avatar={avatar || user.avatar}
+          onAvatarChange={setAvatar}
+        />
+      )}
       <UserForm
         user={user}
         title="Edit User"
         handleSubmit={data => {
-          handleSubmitUpdate({...data, target: data._id})
+          handleSubmitUpdate({...data, target: data._id, avatar})
           navigate('/users')
         }}
       />
